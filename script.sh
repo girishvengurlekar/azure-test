458ad246d755807787de8920386ffed629c89127
Girish Vengurlekar Test <girish.vkar@gmail.com>
2020-09-27 11:03:17 +0000

    2.7

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ sudo -n rm -r deployment
 sudo mkdir deployment
 cd deployment
 DEPLOYMENTPATH=$PWD
-virtualenv2.7 virtualenv
+virtualenv --python=python2.7 virtualenv
 echo $PA
 echo $DEPLOYMENTPATH
 sudo scp -r $PA/.* .
