c8f22abf1e611e4da995d0c863d430db0564a1fe
Girish Vengurlekar Test <girish.vkar@gmail.com>
2020-09-27 09:46:29 +0000

    changed port number

--- script.sh
+++ script.sh
@@ -11,6 +11,6 @@ echo $DEPLOYMENTPATH
 scp -r $PA/.* .
 source virtualenv/bin/activate
 pip install -r requirements.txt
-sudo kill $(sudo lsof -t -i:3000)
+sudo kill $(sudo lsof -t -i:5000)
 python app.py -P 0.0.0.0 &
 
